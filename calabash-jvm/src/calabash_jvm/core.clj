c07e6166cc4bfbdb94dcadaed86dad14a610a0f2
Karl Krukow <karl.krukow@gmail.com>
2012-03-11 13:49:33 +0100

    misc

--- calabash-jvm/src/calabash_jvm/core.clj
+++ calabash-jvm/src/calabash_jvm/core.clj
@@ -2,31 +2,19 @@
   (:require [http.async.client :as http]
             [clojure.data.json :as json]
             [clojure.tools.logging :as lg]
-            [clj-logging-config.log4j :as l4j]))
-
-;; The key is to start swank from emacs as the inferior lisp process instead of calling lein swank from the shell. One way to do this is to use elein (the command is M-x elein-swank). Then you can either inspect the output in the inferior lisp buffer (which is called *elein-swank* in the example of using elein), or execute slime-redirect-inferior-output and have the output inline in the repl. Clojure.contrib.logging is a useful tool for sending log output.
-
+            [clj-logging-config.log4j :as l4j]
+            [calabash-jvm.env :as env]))
 
 
-(defn getenv [str] (System/getenv str))
-
-(def ENDPOINT (or (getenv "DEVICE_ENDPOINT")
-                  "http://localhost:37265"))
-
-(def UUID (getenv "UUID"))
+(def client (http/create-client))
 
 
-(def ^:dynamic *client* (http/create-client))
-
-(defn err [req rsp]
-  (lg/warn "Error: " req rsp))
-
-(defn rpc
-  "Make an rpc"
+(defn rpc-map
+  "TODO doc rpc-map"
   [method-name query & args]
-  (let [data {:method_name method-name
-              :arguments (if args args [])}
-        body {:operation data :query query}
+  (let [body {:query query
+              :operation {:method_name method-name
+                          :arguments (or args [])}}
         response (http/POST
                   *client*
                   (str ENDPOINT "/map")
@@ -38,3 +26,6 @@
       (if (not= "SUCCESS" (:outcome res))
         (err body res)
         (:results res)))))
+
+
+;; The key is to start swank from emacs as the inferior lisp process instead of calling lein swank from the shell. One way to do this is to use elein (the command is M-x elein-swank). Then you can either inspect the output in the inferior lisp buffer (which is called *elein-swank* in the example of using elein), or execute slime-redirect-inferior-output and have the output inline in the repl. Clojure.contrib.logging is a useful tool for sending log output.
