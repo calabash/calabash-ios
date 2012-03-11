c956e73bd91cc88459ec835b385980ccc55abf3e
Karl Krukow <karl.krukow@gmail.com>
2012-03-11 13:49:45 +0100

    misc

---
+++ calabash-jvm/src/calabash_jvm/env.clj
@@ -0,0 +1,8 @@
+(ns calabash-jvm.env)
+
+(defn getenv [str] (System/getenv str))
+
+(def ENDPOINT (or (getenv "DEVICE_ENDPOINT")
+                  "http://localhost:37265"))
+
+(def UUID (getenv "UUID"))
