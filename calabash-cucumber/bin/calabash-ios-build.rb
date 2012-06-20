2cd6a7d2242d0c1b086066489d263db714de2f98
Karl Krukow <karl.krukow@gmail.com>
2012-06-20 20:14:20 +0200

    xcconfig

--- calabash-cucumber/bin/calabash-ios-build.rb
+++ calabash-cucumber/bin/calabash-ios-build.rb
@@ -1,16 +1,49 @@
 
-def build(options={:build_dir=>"Calabash"})
-  raise "not supported yet... coming soon"
+def build(options={:build_dir=>"Calabash",
+                   :configuration => "Debug",
+                   :sdk => "iphonesimulator",
+                   :dstroot => "Calabash/build",
+                   :wrapper_name => "Calabash.app"})
   #Follow Pete's .xcconfig-based approach with zero-config
-  #`xcodebuild -xcconfig Calabash/cal.xcconfig install -configuration Debug -sdk iphonesimulator DSTROOT=Calabash/build WRAPPER_NAME=Calabash.app`
+
+  if !File.exists?("#{options[:build_dir]}/cal.xcconfig")
+      FileUtils.cp(File.join(File.dirname(__FILE__),"cal.xcconfig"),"#{options[:build_dir]}/cal.xcconfig")
+  end
+
+  cmd=["xcodebuild"]
+  cmd << %Q[-xcconfig "#{options[:build_dir]}/cal.xcconfig"]
+  cmd << "install"
+
+  (options[:target] || []).each do |tgt|
+    options << %Q[-target "#{tgt}"]
+  end
+
+  cmd << "-configuration"
+  cmd << %Q["#{options[:configuration]}"]
+
+  cmd << "-sdk"
+  cmd << %Q["#{options[:sdk]}"]
+
+  cmd << %Q[DSTROOT="#{options[:dstroot]}"]
+
+  cmd << %Q[WRAPPER_NAME="#{options[:wrapper_name]}"]
+
+  msg("Calabash Build") do
+    cmd_s = cmd.join(" ")
+    puts cmd_s
+    system(cmd_s)
+  end
 end
 
 def console(options={:script => "irb_ios5.sh"})
   if !File.exists?(".irbrc")
+    puts "Copying calabash-ios .irbrc file to current directory..."
     FileUtils.cp(File.join(@source_dir,".irbrc"), ".")
   end
   if !File.exists?(options[:script])
+    puts "Copying calabash-ios #{options[:script]} file to current directory..."
     FileUtils.cp(File.join(@source_dir,options[:script]), ".")
   end
+  puts "Running irb with ./.irbrc..."
   system("./#{options[:script]}")
 end
--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -1,6 +1,6 @@
 module Calabash
   module Cucumber
-    VERSION = "0.9.66"
+    VERSION = "0.9.67"
     FRAMEWORK_VERSION = "0.9.65"
   end
 end
