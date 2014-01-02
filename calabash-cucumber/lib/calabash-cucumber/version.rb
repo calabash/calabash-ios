4d5afe21301e54f5277eb382e2afb1a9c974b437
Karl Krukow <karl.krukow@gmail.com>
2014-01-02 19:22:16 +0100

    bump versions

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -1,6 +1,6 @@
 module Calabash
   module Cucumber
-    VERSION = '0.9.163.pre9'
-    FRAMEWORK_VERSION = '0.9.163.pre7'
+    VERSION = '0.9.163.pre10'
+    FRAMEWORK_VERSION = '0.9.163.pre10'
   end
 end
