a9634b1a953f84fee8a00502ce1ce1ad9752a645
Joshua Moody <joshuajmoody@gmail.com>
2014-03-10 14:17:09 +0100

    rolls back version to 0.9.168.pre3 and merged 169 changelog back to 168

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -1,6 +1,6 @@
 module Calabash
   module Cucumber
-    VERSION = '0.9.169.pre2'
-    FRAMEWORK_VERSION = '0.9.169.pre2'
+    VERSION = '0.9.168.pre3'
+    FRAMEWORK_VERSION = '0.9.168.pre3'
   end
 end
