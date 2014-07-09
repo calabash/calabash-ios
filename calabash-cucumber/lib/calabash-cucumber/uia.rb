88c5d65a2f040d98eed27852daff3af6f40455e6
Karl Krukow <karl.krukow@gmail.com>
2014-07-09 14:38:59 +0200

    Document the UIA module

--- calabash-cucumber/lib/calabash-cucumber/uia.rb
+++ calabash-cucumber/lib/calabash-cucumber/uia.rb
@@ -6,10 +6,16 @@ require 'calabash-cucumber/utils/logging'
 
 module Calabash
   module Cucumber
+    # Low-level module for interacting directly with UIA
+    # See also {https://developer.apple.com/library/ios/documentation/ToolsLanguages/Reference/UIAElementClassReference/UIAElement/UIAElement.html}
+    # Typically used to interact with System or remote views.
     module UIA
 
       include Calabash::Cucumber::Logging
 
+      # executes raw JavaScript in the UIAutomation environment (using `eval`).
+      # @param {String} command the JavaScript snipplet to execute
+      # @return {Object} the result returned by the UIA process
       def uia(command,options={})
         res = http({:method => :post, :path => 'uia'}, {:command => command}.merge(options))
         res = JSON.parse(res)
@@ -19,6 +25,7 @@ module Calabash
         res['results'].first
       end
 
+      # @!visibility private
       def uia_wait_tap(query, options={})
         res = http({:method => :post, :path => 'uia-tap'}, {:query => query}.merge(options))
         res = JSON.parse(res)
@@ -28,110 +35,185 @@ module Calabash
         res['results']
       end
 
+      # Invoke a Calabash query inside the UIAutomation Calabash engine
+      # Note that this traverses the UIA (accessibility) hierarchy.
+      # @example uia query equivalent of "button marked:'Hello'"
+      #   uia_query :button, marked:'Hello'
+      # @param {Array} queryparts array of segments in the query, e.g., `:button, {marked:'Hello'}`
+      # @return {Array<Hash>} UIAElements matching the query in serialized form.
       def uia_query(*queryparts)
         #TODO escape '\n etc in query
         uia_handle_command(:query, queryparts)
       end
 
+      # @!visibility private
       def uia_query_el(*queryparts)
         #TODO escape '\n etc in query
         uia_handle_command(:queryEl, queryparts)
       end
 
+      # Invoke a Calabash query inside the UIAutomation Calabash engine - includes all UIAWindows.
+      # Note that this traverses the UIA (accessibility) hierarchy.
+      # @example uia query equivalent of "button marked:'Hello'"
+      #   uia_query_windows :button
+      # @param {Array} queryparts array of segments in the query, e.g., `:button, {marked:'Hello'}`
+      # @return {Array<Hash>} UIAElements matching the query in serialized form.
       def uia_query_windows(*queryparts)
         #TODO escape '\n etc in query
         uia_handle_command(:queryWindows, queryparts)
       end
 
+      # Invoke a Calabash query inside the UIAutomation Calabash engine - includes all UIAWindows.
+      # Note that this traverses the UIA (accessibility) hierarchy.
+      # @example uia equivalent of `identifier "button"`
+      #   uia_names :button
+      #   # returns
+      #   [
+      #     "Browse",
+      #     "UINavigationBarBackIndicatorDefault.png",
+      #     "16h",
+      #     "reader postaction comment blue",
+      #     "16h",
+      #     "52",
+      #     "17h",
+      #     "10",
+      #     "Reader",
+      #     "Notifications",
+      #     "Me",
+      #     "New Post"
+      #   ]
+      # @param {Array} queryparts array of segments in the query, e.g., `:button, {marked:'Hello'}`
+      # @return {Array<String>} "names" (accessibilityIdentifier) of UIAElements matching the query.
       def uia_names(*queryparts)
         #TODO escape '\n etc in query
         uia_handle_command(:names, queryparts)
       end
 
+      # Advanced method used to invoke UIAutomation JavaScript methods on objects found via Calabash queries
+      # @see #uia_query
+      # @example Calling UIAButton.isVisible() - {https://developer.apple.com/library/ios/documentation/ToolsLanguages/Reference/UIAElementClassReference/UIAElement/UIAElement.html}
+      #   uia_call [:button, {marked:'New Post'}], :isVisible
+      # @example Advanced example that chains calls and uses arguments
+      #   uia_call [:view, marked:'New Post'], {withName:"New Post"}, :toString, {charAt:0}
+      #
+      # @param {Array} args_arr array describing the query, e.g., [:button, {marked:'foo'}]
+      # @param {Array} opts optional arguments specifying a chained sequence of method calls (see example)
+      def uia_call(args_arr, *opts)
+        uia_call_method(:queryEl, args_arr, *opts)
+      end
+
+      # Similar to `uia_call` but searches all windows
+      # @see #uia_call
+      def uia_call_windows(args_arr, *opts)
+        uia_call_method(:queryElWindows, args_arr, *opts)
+      end
+
+      # @!visibility private
       def uia_tap(*queryparts)
         uia_handle_command(:tap, queryparts)
       end
 
+      # @!visibility private
       def uia_tap_mark(mark)
         uia_handle_command(:tapMark, mark)
       end
 
+      # @!visibility private
       def uia_tap_offset(offset)
         uia_handle_command(:tapOffset, offset)
       end
 
+      # @!visibility private
       def uia_double_tap(*queryparts)
         uia_handle_command(:doubleTap, queryparts)
       end
 
+      # @!visibility private
       def uia_double_tap_mark(mark)
         uia_double_tap(:view, {:marked => mark})
       end
 
+      # @!visibility private
       def uia_double_tap_offset(offset)
         uia_handle_command(:doubleTapOffset, offset)
       end
 
+      # @!visibility private
       def uia_two_finger_tap(*queryparts)
         uia_handle_command(:twoFingerTap, queryparts)
       end
 
+      # @!visibility private
       def uia_two_finger_tap_offset(offset)
         uia_handle_command(:twoFingerTapOffset, offset)
       end
 
+      # @!visibility private
       def uia_flick_offset(from, to)
         uia_handle_command(:flickOffset, from, to)
       end
 
+      # @!visibility private
       def uia_touch_hold(duration, *queryparts)
         uia_handle_command(:touchHold, duration, queryparts)
       end
 
+      # @!visibility private
       def uia_touch_hold_offset(duration, offset)
         uia_handle_command(:touchHoldOffset, duration, offset)
       end
 
+      # @!visibility private
       def uia_pan(from_q, to_q)
         uia_handle_command(:pan, from_q, to_q)
       end
 
+      # @!visibility private
       def uia_pan_offset(from, to, options)
         uia_handle_command(:panOffset, from, to, options)
       end
 
+      # @!visibility private
       def uia_swipe(*queryparts)
         uia_handle_command(:swipe, queryparts)
       end
 
+      # @!visibility private
       def uia_swipe_offset(offset, options)
         uia_handle_command(:swipeOffset, offset, options)
       end
 
+      # @!visibility private
       def uia_pinch(*queryparts)
         uia_handle_command(:pinch, queryparts)
       end
 
+      # @!visibility private
       def uia_pinch_offset(in_or_out, offset, duration)
         uia_handle_command(:pinchOffset, in_or_out, offset, duration)
       end
 
+      # @!visibility private
       def uia_scroll_to(*queryparts)
         uia_handle_command(:scrollTo, queryparts)
       end
 
+      # @!visibility private
       def uia_element_exists?(*queryparts)
         uia_handle_command(:elementExists, queryparts)
       end
 
+      # @!visibility private
       def uia_element_does_not_exist?(*queryparts)
         uia_handle_command(:elementDoesNotExist, queryparts)
       end
 
+      # @!visibility private
       def uia_screenshot(name)
         uia_handle_command(:screenshot, name)
       end
 
+      # @!visibility private
       def uia_type_string(string, opt_text_before='', escape=true)
         if escape && string.index(/\\/)
           indexes = string.enum_for(:scan,/\\/).map { Regexp.last_match.begin(0) }
@@ -146,10 +228,12 @@ module Calabash
         status
       end
 
+      # @!visibility private
       def uia_enter
         uia_handle_command(:enter)
       end
 
+      # @!visibility private
       def uia_set_location(options)
         validate_hash_is_location!(options)
         if options[:place]
@@ -164,18 +248,12 @@ module Calabash
         uia_handle_command(:setLocation, loc_data)
       end
 
+      # @!visibility private
       def uia_send_app_to_background(secs)
         uia_handle_command(:deactivate, secs)
       end
 
-      def uia_call(args_arr, *opts)
-        uia_call_method(:queryEl, args_arr, *opts)
-      end
-
-      def uia_call_windows(args_arr, *opts)
-        uia_call_method(:queryElWindows, args_arr, *opts)
-      end
-
+      # @!visibility private
       def uia_call_method(cmd, args_arr, *opts)
         if opts.empty?
           return uia_handle_command(cmd, args_arr)
@@ -213,6 +291,7 @@ module Calabash
 
       end
 
+      # @!visibility private
       def uia_handle_command(cmd, *query_args)
         command = uia_serialize_command(cmd, *query_args)
         if debug_logging?
@@ -223,17 +302,20 @@ module Calabash
         uia_result(s)
       end
 
+      # @!visibility private
       def uia_serialize_command(cmd, *query_args)
         args = uia_serialize_arguments(query_args)
         %Q[uia.#{cmd}(#{args.join(', ')})]
       end
 
+      # @!visibility private
       def uia_serialize_arguments(args)
         args.map do |part|
           uia_serialize_argument(part)
         end
       end
 
+      # @!visibility private
       def uia_serialize_argument(part)
         if part.is_a?(String)
           "'#{escape_uia_string(part)}'"
@@ -242,6 +324,7 @@ module Calabash
         end
       end
 
+      # @!visibility private
       def escape_uia_string(string)
         #TODO escape '\n in query
         escape_quotes string
@@ -249,6 +332,7 @@ module Calabash
 
       # <b>DEPRECATED:</b> Use <tt>uia("...javascript..", options)</tt> instead.
       # deprecated because the method signature is poor
+      # @!visibility private
       def send_uia_command(opts ={})
 
         # TODO formally deprecate send_uia_command with _deprecated function
