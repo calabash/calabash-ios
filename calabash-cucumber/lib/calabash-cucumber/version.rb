aeccfe50e0f42edd4b92ad8cb750ad41955f4df7
Joshua Moody <joshuajmoody@gmail.com>
2014-07-11 17:08:00 +0200

    constants should be doc'd

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -1,7 +1,15 @@
 require 'calabash-cucumber/utils/logging'
+
 module Calabash
   module Cucumber
+
+    # @!visibility public
+    # The Calabash iOS gem version.
     VERSION = '0.10.0.pre1'
+
+    # @!visibility public
+    # The minimum required version of the calabash.framework or, for Xamarin
+    # users, the Calabash component.
     MIN_SERVER_VERSION = '0.10.0.pre1'
 
     def self.const_missing(const_name)
