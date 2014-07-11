ecfaa0b04c8f98231d90b07e848510b88c48bc96
Joshua Moody <joshuajmoody@gmail.com>
2014-07-11 23:16:39 +0200

    attrs should be doc'd

--- calabash-cucumber/lib/calabash-cucumber/device.rb
+++ calabash-cucumber/lib/calabash-cucumber/device.rb
@@ -30,14 +30,91 @@ module Calabash
       # @!visibility private
       GESTALT_IPOD = 'iPod'
 
+      # @!attribute [r] endpoint
+      # The http address of this device.
+      # @example
+      #  http://192.168.0.2:37265
+      # @return [String] an ip address with port number.
       attr_reader :endpoint
+
+      # The device family of this device.
+      #
+      # @note Also know as the form factor.
+      #
+      # @example
+      #  # will be one of
+      #  iPhone
+      #  iPod
+      #  iPad
+      #
+      # @!attribute [r] device_family
+      # @return [String] the device family
       attr_reader :device_family
-      attr_reader :simulator_details, :ios_version
+
+      # @!visibility private
+      # @attribute [r] simulator_details
+      # @return [String] Additional details about the simulator.  If this device
+      #  is a physical device, returns nil.
+      attr_reader :simulator_details
+
+      # The `major.minor.[.patch]` version of iOS that is running on this device.
+      #
+      # @example
+      #  7.1
+      #  6.1.2
+      #  5.1.1
+      #
+      # @attribute [r] ios_version
+      # @return [String] the version of the iOS that is running on this device
+      attr_reader :ios_version
+
+      # The hardware architecture of this device.  Also known as the chip set.
+      #
+      # @example
+      #  # simulator
+      #  i386
+      #  x86_64
+      #
+      # @example
+      #  # examples from physical devices
+      #  armv6
+      #  armv7s
+      #  arm64
+      #
+      # @attribute [r] system
+      # @return [String] the hardware architecture of this device.
+      #  this device.
       attr_reader :system
+
+      # The version of the embedded Calabash server that is running in the
+      # app under test on this device.
+      #
+      # @example
+      #  0.9.168
+      #  0.10.0.pre1
+      #
+      # @attribute [r] server_version
+      # @return [String] the major.minor.patch[.pre\d] version of the embedded
+      #  Calabash server
       attr_reader :server_version
+
+      # Indicates whether or not the app under test on this device is an
+      #  iPhone-only app that is being emulated on an iPad.
+      #
+      # @note If the `1x` or `2x` button is visible, then the app is being
+      #  emulated.
+      #
+      # @attribute [r] iphone_app_emulated_on_ipad
+      # @return [Boolean] `true` iff the app under test is emulated
       attr_reader :iphone_app_emulated_on_ipad
+
+      # Indicates whether or not this device has a 4in screen.
+      # @attribute [r] iphone_4in
+      # @return [Boolean] `true` iff this device has a 4in screen.
       attr_reader :iphone_4in
 
+      # @!attribute [rw] udid
+      # @return [String] The udid of this device.
       attr_accessor :udid
 
       def initialize (endpoint, version_data)
