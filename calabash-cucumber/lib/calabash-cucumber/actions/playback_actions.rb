23ce71c294f3b50943788bccf88fb701dcd7d7c0
Joshua Moody <joshuajmoody@gmail.com>
2014-07-18 20:08:20 +0200

    should be pendatic about visibility private

--- calabash-cucumber/lib/calabash-cucumber/actions/playback_actions.rb
+++ calabash-cucumber/lib/calabash-cucumber/actions/playback_actions.rb
@@ -8,31 +8,37 @@ class Calabash::Cucumber::PlaybackActions
   include Calabash::Cucumber::ConnectionHelpers
   include Calabash::Cucumber::QueryHelpers
 
-
+  # @!visibility private
   def touch(options)
     playback('touch', options)
   end
 
+  # @!visibility private
   def wait_tap(options)
     touch(options)
   end
 
+  # @!visibility private
   def double_tap(options)
     playback('double_tap', options)
   end
 
+  # @!visibility private
   def two_finger_tap(*args)
     raise error_message('two_finger_tap')
   end
 
+  # @!visibility private
   def flick(*args)
     raise error_message('flick')
   end
 
+  # @!visibility private
   def touch_hold(options)
     playback('touch_hold', options)
   end
 
+  # @!visibility private
   def swipe(dir, options={})
     current_orientation = options[:status_bar_orientation].to_sym
     if current_orientation == :left
@@ -81,10 +87,12 @@ class Calabash::Cucumber::PlaybackActions
 
   end
 
+  # @!visibility private
   def pan(from,to,options={})
     interpolate 'pan', options.merge(:start => from, :end => to)
   end
 
+  # @!visibility private
   def pinch(in_out,options)
     file = "pinch_in"
     if in_out==:out
@@ -93,12 +101,13 @@ class Calabash::Cucumber::PlaybackActions
     playback(file, options)
   end
 
+  # @!visibility private
   def send_app_to_background(secs)
     raise 'Not implemented when running without instruments / UIA'
   end
 
-
   private
+  # @!visibility private
   def error_message(gesture)
     "Gesture: '#{gesture}' not supported unless running with instruments."
   end
