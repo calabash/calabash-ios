73dceb1f4a0213165d320053c5e10bfbe4b726fe
Joshua Moody <joshuajmoody@gmail.com>
2014-07-24 15:46:55 +0200

    should support ruby >= 1.9, not _only_ ruby 1.9.*

---
+++ script/install-gem-ci.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), 'ci_helpers'))
+
+working_directory = File.expand_path(File.join(File.dirname(__FILE__), '..', 'calabash-cucumber'))
+
+Dir.chdir working_directory do
+
+  do_system('rake install',
+            {:pass_msg => 'successfully used rake to install the gem',
+             :fail_msg => 'could not install the gem with rake'})
+
+end
