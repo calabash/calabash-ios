3f6d6da7ad71398d07eeb31245e82e264cef07b7
Karl Krukow <karl.krukow@gmail.com>
2014-07-24 14:18:42 +0200

    Include frank-calabash.rb

---
+++ calabash-cucumber/lib/frank-calabash.rb
@@ -0,0 +1,13 @@
+# stubs for documentation
+require 'calabash-cucumber/core'
+require 'calabash-cucumber/launcher'
+
+# base module for Calabash iOS
+module Frank
+  # base sub module
+  # todo in 1.0 release consider changing Cucumber module to iOS module
+  module Calabash
+
+
+  end
+end
