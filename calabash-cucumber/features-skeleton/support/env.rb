857ce6c7685a12598bc1b60796d2644a7f4eb948
Joshua Moody <joshuajmoody@gmail.com>
2014-08-14 01:35:19 +0200

    WIP instructions how _not_ to import predefined steps

--- calabash-cucumber/features-skeleton/support/env.rb
+++ calabash-cucumber/features-skeleton/support/env.rb
@@ -1,1 +1,20 @@
+# WIP - needs better text and testing
+#
+# @see https://github.com/calabash/calabash-ios/issues/458
+
+# This will import the calabash pre-defined steps.
+#
+# Once you have some experience with Calabash and Cucumber, we recommend that
+# not use pre-defined steps.  Instead, you should write Steps that are specific
+# to your application.
+#
+# Imports calabash-ios pre-defined steps.
 require 'calabash-cucumber/cucumber'
+
+# If you don't want to use the pre-defined steps,
+# delete the 'require' above and uncomment the
+# requires below.
+
+# Imports calabash-ios without pre-defined steps.
+# require 'calabash-cucumber/wait_helpers'
+# require 'calabash-cucumber/operations'
