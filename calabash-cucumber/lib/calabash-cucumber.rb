69b5a63ad249de4a118db2f6560701ddc28940d7
Joshua Moody <joshuajmoody@gmail.com>
2014-08-14 17:09:50 +0200

    docs should be capitialized sentences [skip ci]

--- calabash-cucumber/lib/calabash-cucumber.rb
+++ calabash-cucumber/lib/calabash-cucumber.rb
@@ -11,10 +11,10 @@ require 'calabash-cucumber/utils/logging'
 
 # stubs for documentation
 
-# base module for Calabash iOS
+# The base module for Calabash iOS.
 module Calabash
-  # base sub module
   # todo in 1.0 release consider changing Cucumber module to iOS module
+  # The base sub module for Calabash iOS
   module Cucumber
 
   end
