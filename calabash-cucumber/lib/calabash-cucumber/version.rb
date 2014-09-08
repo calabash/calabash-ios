6d9104d4b8cee6fe55b34d3d1cbc848f50eac27f
Joshua Moody <joshuajmoody@gmail.com>
2014-09-09 00:44:06 +0200

    Bump version to 0.10.0
    Hurray!

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -5,7 +5,7 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = '0.10.0.pre8'
+    VERSION = '0.10.0'
 
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
