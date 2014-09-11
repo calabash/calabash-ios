4ea38862c35f1b65f1f269bb23c1c255f0a4eed8
Joshua Moody <joshuajmoody@gmail.com>
2014-09-11 16:00:36 +0200

    Fixes server_version_from_server spec

--- calabash-cucumber/spec/launcher_spec.rb
+++ calabash-cucumber/spec/launcher_spec.rb
@@ -292,8 +292,8 @@ describe 'Calabash Launcher' do
         # We can't stand up the server, so we'll create a device and ask for
         # its version.  It is the best we can do for now.
         device = Resources.shared.device_for_mocking
-        actual = launcher.server_version_from_server
         launcher.device = device
+        actual = launcher.server_version_from_server
         expect(actual).not_to be == nil
         expect(RunLoop::Version.new(actual).to_s).to be == '0.10.0'
       end
