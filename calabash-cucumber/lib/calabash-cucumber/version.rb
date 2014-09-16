a6293df443ee9611d0455538274afab6d43fbc73
Joshua Moody <joshuajmoody@gmail.com>
2014-09-16 19:50:05 +0200

    Bump the min server version to 0.10.1

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -11,7 +11,7 @@ module Calabash
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.10.0'
+    MIN_SERVER_VERSION = '0.10.1'
 
     # @!visibility private
     def self.const_missing(const_name)
