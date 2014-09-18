1f8e555933225ea0bc4fa1992807756cc50e310b
Joshua Moody <joshuajmoody@gmail.com>
2014-09-18 06:37:50 +0200

    Moves Version class out of version.rb
    The gemspec reads lib/version.rb at install time.  Version.rb
    require'd run_loop/version which meant that run-loop needed to be
    installed _before_ the gemspec could be resolved and the gem
    installed.

--- calabash-cucumber/lib/calabash-cucumber.rb
+++ calabash-cucumber/lib/calabash-cucumber.rb
@@ -8,6 +8,7 @@ require 'calabash-cucumber/version'
 require 'calabash-cucumber/date_picker'
 require 'calabash-cucumber/ipad_1x_2x'
 require 'calabash-cucumber/utils/logging'
+require 'calabash-cucumber/deprecated'
 
 # stubs for documentation
 
