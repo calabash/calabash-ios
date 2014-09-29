5ab7d474182b7b72e3e446816c6fda17770828bb
Joshua Moody <joshuajmoody@gmail.com>
2014-09-29 22:19:20 +0200

    Regr. tests calabash_exit targeting sims

--- calabash-cucumber/spec/core_spec.rb
+++ calabash-cucumber/spec/core_spec.rb
@@ -16,7 +16,9 @@ describe Calabash::Cucumber::Core do
   }
 
   describe '#calabash_exit' do
-    describe 'current Xcode targeting a simulator' do
+    describe 'targeting simulators' do
+      let(:launcher) { Calabash::Cucumber::Launcher.new }
+      let(:core_instance) { CoreIncluded.new }
       it "Xcode #{Resources.shared.current_xcode_version}" do
         sim_control = RunLoop::SimControl.new
         options =
@@ -26,11 +28,37 @@ describe Calabash::Cucumber::Core do
                     :sim_control => sim_control,
                     :launch_retries => Resources.shared.travis_ci? ? 5 : 2
               }
-
-        launcher = Calabash::Cucumber::Launcher.new
         launcher.relaunch(options)
         expect(launcher.run_loop).not_to be == nil
-        expect { CoreIncluded.new.calabash_exit }.not_to raise_error
+        expect { core_instance.calabash_exit }.not_to raise_error
+      end
+
+      describe 'Xcode regression' do
+        xcode_installs = Resources.shared.alt_xcodes_gte_xc51_hash
+        if xcode_installs.empty?
+          it 'no alternative Xcode installs' do
+            expect(true).to be == true
+          end
+        else
+          xcode_installs.each do |install_hash|
+            version = install_hash[:version]
+            path = install_hash[:path]
+            it "Xcode #{version} @ #{path}" do
+              ENV['DEVELOPER_DIR'] = path
+              sim_control = RunLoop::SimControl.new
+              options =
+                    {
+                          :app => Resources.shared.app_bundle_path(:lp_simple_example),
+                          :device_target => 'simulator',
+                          :sim_control => sim_control,
+                          :launch_retries => Resources.shared.travis_ci? ? 5 : 2
+                    }
+              launcher.relaunch(options)
+              expect(launcher.run_loop).not_to be == nil
+              expect { core_instance.calabash_exit }.not_to raise_error
+            end
+          end
+        end
       end
     end
   end
