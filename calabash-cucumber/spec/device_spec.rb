7ca01afa72698bf2ab8ea7950102e855e5342bb9
Joshua Moody <joshuajmoody@gmail.com>
2014-10-16 00:53:45 +0200

    Device spec should use Resources server_version

--- calabash-cucumber/spec/device_spec.rb
+++ calabash-cucumber/spec/device_spec.rb
@@ -1,33 +1,7 @@
 describe Calabash::Cucumber::Device do
 
   # noinspection RubyStringKeysInHashInspection
-  let(:simulator_data) {
-    {
-          'app_version' => '1.0',
-          'outcome' => 'SUCCESS',
-          'app_id' => 'com.xamarin.chou-cal',
-          'simulator_device' => 'iPhone',
-          'version' => '0.11.0',
-          'app_name' => 'chou-cal',
-          'iphone_app_emulated_on_ipad' => false,
-          '4inch' => true,
-          'git' => {
-                'remote_origin' => 'git@github.com:calabash/calabash-ios-server.git',
-                'branch' => 'develop',
-                'revision' => '652b20b'
-          },
-          'screen_dimensions' => {
-                'scale' => 2,
-                'width' => 640,
-                'sample' => 1,
-                'height' => 1136
-          },
-          'iOS_version' => '7.1',
-          'system' => 'x86_64',
-          'simulator' => 'CoreSimulator 110.2 - Device: iPhone 5 - Runtime: iOS 7.1 (11D167) - DeviceType: iPhone 5'
-    }
-  }
-
+  let(:simulator_data) { Resources.shared.server_version :simulator }
   let(:endpoint) { 'http://localhost:37265' }
 
   describe '#ios8?' do
