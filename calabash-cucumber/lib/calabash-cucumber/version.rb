4a8bbe7923790d056026266409ccd4ab8cbe70ac
Joshua Moody <joshuajmoody@gmail.com>
2014-10-15 17:24:29 +0200

    Set minimum server version to 0.11.1

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -8,6 +8,6 @@ module Calabash
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.11.0'
+    MIN_SERVER_VERSION = '0.11.1'
   end
 end
