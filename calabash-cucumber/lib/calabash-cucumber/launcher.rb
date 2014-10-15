59698f0d92b946f2b74d76721d54799f6481e81f
Joshua Moody <joshuajmoody@gmail.com>
2014-10-15 21:21:02 +0200

    Call 'strings' in the context of xcrun

--- calabash-cucumber/lib/calabash-cucumber/launcher.rb
+++ calabash-cucumber/lib/calabash-cucumber/launcher.rb
@@ -961,7 +961,7 @@ class Calabash::Cucumber::Launcher
 
     server_version = nil
     exe_paths.each do |path|
-      server_version_string = `strings "#{path}" | grep -E 'CALABASH VERSION'`.chomp!
+      server_version_string = `xcrun strings "#{path}" | grep -E 'CALABASH VERSION'`.chomp!
       if server_version_string
         server_version = server_version_string.split(' ').last
         break
