705fa241bebca4971e003983deeb25a9013bdb87
Joshua Moody <joshuajmoody@gmail.com>
2014-10-24 23:14:55 +0200

    Add LPSimpleExample-cal.app for testing

--- calabash-cucumber/spec/resources.rb
+++ calabash-cucumber/spec/resources.rb
@@ -26,7 +26,7 @@ class Resources
   def app_bundle_path(bundle_name)
     case bundle_name
       when :lp_simple_example
-        return @lp_cal_app_bundle_path ||= File.join(self.resources_dir, 'enable-accessibility', 'LPSimpleExample-cal.app')
+        return @lp_cal_app_bundle_path ||= File.join(self.resources_dir, 'LPSimpleExample-cal.app')
       when :chou
         return @chou_app_bundle_path ||= File.join(self.resources_dir, 'chou.app')
       else
