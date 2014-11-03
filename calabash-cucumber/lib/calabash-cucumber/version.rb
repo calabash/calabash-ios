c7a39a2a1e3969fe827a3d034af519b5bf55b510
Karl Krukow <karl.krukow@gmail.com>
2014-11-03 14:33:37 +0100

    Update server for 0.11.4.pre1

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,11 +3,11 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = '0.11.3'
+    VERSION = '0.11.4.pre1'
 
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.11.3'
+    MIN_SERVER_VERSION = '0.11.4.pre1'
   end
 end
