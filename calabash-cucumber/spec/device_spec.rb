8314e9d912c2e5de1039505f0d33e6e95d728f32
Michael John Kirk <michael.john.kirk@gmail.com>
2014-11-03 22:10:28 +0100

    split integration specs into separate directory

--- calabash-cucumber/spec/device_spec.rb
+++ calabash-cucumber/spec/device_spec.rb
@@ -1,19 +1,4 @@
 describe Calabash::Cucumber::Device do
-
-  before(:each) do
-    ENV.delete('DEVELOPER_DIR')
-    ENV.delete('DEBUG')
-    ENV.delete('DEBUG_UNIX_CALLS')
-    RunLoop::SimControl.terminate_all_sims
-  end
-
-  after(:each) {
-    ENV.delete('DEVELOPER_DIR')
-    ENV.delete('DEBUG')
-    ENV.delete('DEBUG_UNIX_CALLS')
-    RunLoop::SimControl.terminate_all_sims
-  }
-
   # noinspection RubyStringKeysInHashInspection
   let(:simulator_data) { Resources.shared.server_version :simulator }
   let(:endpoint) { 'http://localhost:37265' }
@@ -32,89 +17,15 @@ describe Calabash::Cucumber::Device do
   end
 
   describe '#iphone_4in?' do
-    let(:device)  do
-      options = {
-            :app => Resources.shared.app_bundle_path(:lp_simple_example),
-            :device_target =>  device_target,
-            :sim_control => RunLoop::SimControl.new,
-            :launch_retries => Resources.shared.travis_ci? ? 5 : 2
-      }
-      launcher = Calabash::Cucumber::Launcher.new
-      launcher.relaunch(options)
-      launcher.device
+    let(:device) { Calabash::Cucumber::Device.new(double('end_point'), version_data) }
+    subject { device.iphone_4in? }
+    context 'when server says it is 4"' do
+      let(:version_data) { Resources.shared.server_version(:simulator).merge({'4inch' => true}) }
+      it { is_expected.to be_truthy }
     end
-
-    describe 'regression' do
-      xcode_installs = Resources.shared.supported_xcode_version_paths
-      if xcode_installs.empty?
-        it 'no alternative versions of Xcode found' do
-          expect(true).to be == true
-        end
-      else
-        subject { device.iphone_4in? }
-        xcode_installs.each do |developer_dir|
-          context "#{developer_dir}" do
-            before do
-              ENV['DEVELOPER_DIR'] = developer_dir
-            end
-
-            context 'device is an 4in iphone' do
-              let(:device_target) {
-                xcode_tools = RunLoop::XCTools.new
-                if xcode_tools.xcode_version_gte_61?
-                  'iPhone 5 (8.1 Simulator)'
-                elsif xcode_tools.xcode_version_gte_6?
-                  'iPhone 5 (8.0 Simulator)'
-                else
-                  'iPhone Retina (4-inch) - Simulator - iOS 7.1'
-                end
-              }
-              it { should be == true }
-            end
-            context 'device is a 3.5" iphone' do
-              let(:device_target) {
-                xcode_tools = RunLoop::XCTools.new
-                if xcode_tools.xcode_version_gte_61?
-                  'iPhone 4s (8.1 Simulator)'
-                elsif xcode_tools.xcode_version_gte_6?
-                  'iPhone 4s (8.0 Simulator)'
-                else
-                  'iPhone Retina (3.5-inch) - Simulator - iOS 7.1'
-                end
-              }
-              it { should be == false }
-            end
-            context 'device is an iphone 6' do
-              let(:device_target) {
-                xcode_tools = RunLoop::XCTools.new
-                if xcode_tools.xcode_version_gte_61?
-                  'iPhone 6 (8.1 Simulator)'
-                elsif xcode_tools.xcode_version_gte_6?
-                  'iPhone 6 (8.0 Simulator)'
-                else
-                  # iPhone 6 does not exist on Xcode < 6
-                  nil
-                end
-              }
-              it { should be == false if device_target }
-            end
-            context 'device is an iphone 6+' do
-              let(:device_target) {
-                xcode_tools = RunLoop::XCTools.new
-                if xcode_tools.xcode_version_gte_61?
-                  'iPhone 6 Plus (8.1 Simulator)'
-                elsif xcode_tools.xcode_version_gte_6?
-                  'iPhone 6 Plus (8.0 Simulator)'
-                else
-                  # iPhone 6 does not exist on Xcode < 6
-                  nil
-                end
-              }
-              it { should be == false if device_target }
-            end
-          end
-        end
-      end
+    context 'when server says it is not 4"' do
+      let(:version_data) { Resources.shared.server_version(:simulator).merge({'4inch' => false}) }
+      it { is_expected.not_to be_truthy }
     end
   end
 end
