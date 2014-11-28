f3c152348872a485f443b07fca59ab50b6090e6d
Karl Krukow <karl.krukow@gmail.com>
2014-11-29 00:08:33 +0100

    Bump run loop and version to 0.11.5.pre3

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,11 +3,11 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = '0.11.5.pre4'
+    VERSION = '0.11.5.pre3'
 
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.11.5.pre2'
+    MIN_SERVER_VERSION = '0.11.5.pre3'
   end
 end
