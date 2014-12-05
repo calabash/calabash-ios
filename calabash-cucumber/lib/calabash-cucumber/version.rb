2e6ae8fc212da2500d3c2f491285832eec599db5
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-05 13:52:53 +0100

    Remove version

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -1,10 +1,5 @@
 module Calabash
   module Cucumber
-
-    # @!visibility public
-    # The Calabash iOS gem version.
-    VERSION = '0.11.4'
-
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
