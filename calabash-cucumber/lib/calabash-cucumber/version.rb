f5088e64156e2094756800c0d7fd069ccfbe5550
Karl Krukow <karl.krukow@gmail.com>
2015-01-20 12:50:04 +0100

    Bump RunLoop and server

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,11 +3,11 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = '0.12.1'
+    VERSION = '0.12.2'
 
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.12.0'
+    MIN_SERVER_VERSION = '0.12.2'
   end
 end
