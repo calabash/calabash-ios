ae9abb72033b581d12b7c75ecb44757669e5cd4f
Joshua Moody <joshuajmoody@gmail.com>
2015-02-25 21:20:24 +0100

    Bump min server version and gem version

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,11 +3,11 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = '0.12.3'
+    VERSION = '0.13.0'
 
     # @!visibility public
     # The minimum required version of the calabash.framework or, for Xamarin
     # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.12.3'
+    MIN_SERVER_VERSION = '0.13.0'
   end
 end
