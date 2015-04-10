b35d923af322431d063d2188a9ae318c9b4ced54
Joshua Moody <joshuajmoody@gmail.com>
2015-04-10 08:37:51 +0200

    Simplify #calabash_exit spec

--- calabash-cucumber/spec/integration/core/calabash_exit_spec.rb
+++ calabash-cucumber/spec/integration/core/calabash_exit_spec.rb
@@ -26,49 +26,40 @@ describe Calabash::Cucumber::Core do
       expect(launcher.run_loop).not_to be == nil
       expect { core_instance.calabash_exit }.not_to raise_error
     end
-  end
 
-  unless Luffa::Environment.travis_ci?
-    describe 'targeting physical devices' do
-      if !Luffa::IDeviceInstaller.ideviceinstaller_available?
-        it 'device testing requires ideviceinstaller to be available in the PATH' do
-          expect(true).to be == true
-        end
-      else
+    unless Luffa::Environment.travis_ci? &&
+          Luffa::IDeviceInstaller.ideviceinstaller_available? &&
+          !Resources.shared.physical_devices_for_testing(RunLoop::XCTools.new).empty?
+
+      describe 'targeting physical devices' do
         sim_control = RunLoop::SimControl.new
         xcode_tools = sim_control.xctools
         xcode_version = xcode_tools.xcode_version
+        Resources.shared.physical_devices_for_testing(xcode_tools).each do |device|
+          if Luffa::Xcode::ios_version_incompatible_with_xcode_version?(device.version, xcode_version)
+            it "Skipping #{device.name} iOS #{device.version} with Xcode #{xcode_tools} - combination not supported" do
+              expect(true).to be == true
+            end
+          else
+            it "on #{device.name} iOS #{device.version} Xcode #{xcode_version}" do
+              stub_env('DEVICE_ENDPOINT', "http://#{device.name}.local:37265")
 
-        physical_devices = Resources.shared.physical_devices_for_testing(xcode_tools)
-        if physical_devices.empty?
-          it 'no physical devices available' do expect(true).to be_truthy end
-        else
-          physical_devices.each do |device|
-            if Luffa::Xcode::ios_version_incompatible_with_xcode_version?(device.version, xcode_version)
-              it "Skipping #{device.name} iOS #{device.version} with Xcode #{xcode_version} - combination not supported" do
-                expect(true).to be == true
-              end
-            else
-              it "on #{device.name} iOS #{device.version} Xcode #{xcode_version}" do
-                stub_env('DEVICE_ENDPOINT', "http://#{device.name}.local:37265")
+              options =
+                    {
+                          :bundle_id => Resources.shared.bundle_id,
+                          :udid => device.udid,
+                          :device_target => device.udid,
+                          :sim_control => sim_control,
+                          :launch_retries => Luffa::Retry.instance.launch_retries
+                    }
 
-                options =
-                          {
-                                :bundle_id => Resources.shared.bundle_id,
-                                :udid => device.udid,
-                                :device_target => device.udid,
-                                :sim_control => sim_control,
-                                :launch_retries => Luffa::Retry.instance.launch_retries
-                          }
+              expect {
+                    Resources.shared.ideviceinstaller.install(device.udid)
+              }.to_not raise_error
 
-                expect {
-                  Resources.shared.ideviceinstaller.install(device.udid)
-                }.to_not raise_error
-
-                launcher.relaunch(options)
-                expect(launcher.run_loop).not_to be == nil
-                expect { core_instance.calabash_exit }.not_to raise_error
-              end
+              launcher.relaunch(options)
+              expect(launcher.run_loop).not_to be == nil
+              expect { core_instance.calabash_exit }.not_to raise_error
             end
           end
         end
