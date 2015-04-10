632e39909357261a04fc25ca08d7a3ce0c788f05
Joshua Moody <joshuajmoody@gmail.com>
2015-04-10 13:33:16 +0200

    Bump server version to 0.14.0

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -6,8 +6,7 @@ module Calabash
     VERSION = '0.14.0'
 
     # @!visibility public
-    # The minimum required version of the calabash.framework or, for Xamarin
-    # users, the Calabash component.
-    MIN_SERVER_VERSION = '0.13.0'
+    # The minimum required version of the Calabash embedded server.
+    MIN_SERVER_VERSION = '0.14.0'
   end
 end
