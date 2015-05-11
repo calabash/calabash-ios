bb04def2c9da31409921f947880de917e9390611
Joshua Moody <joshuajmoody@gmail.com>
2015-05-11 20:23:39 +0200

    Don't rescue Exception in simulator_launcher
    No reason to test, this class is never used now that we
    launch with run_loop + instruments.

--- calabash-cucumber/lib/calabash-cucumber/launch/simulator_launcher.rb
+++ calabash-cucumber/lib/calabash-cucumber/launch/simulator_launcher.rb
@@ -459,8 +459,6 @@ module Calabash
                   begin
                     connected = (ping_app == '200')
                     break if connected
-                  rescue Exception => e
-                    # nop
                   ensure
                     sleep 1 unless connected
                   end
