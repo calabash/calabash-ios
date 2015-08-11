c9963a640c09a85f12f70a27d511b193decc862d
Ashish Tyagi <ashisht@thoughtworks.com>
2015-08-11 19:16:10 +0530

    changed version to 0.14.4

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,7 +3,7 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = '0.14.3'
+    VERSION = '0.14.4'
 
     # @!visibility public
     # The minimum required version of the Calabash embedded server.
