13b21f81717a12c6ee4011a17241a1e9638fa0b2
Joshua Moody <joshuajmoody@gmail.com>
2015-09-17 01:37:43 +0200

    Fixup device integration specs

--- calabash-cucumber/spec/integration/device_spec.rb
+++ calabash-cucumber/spec/integration/device_spec.rb
@@ -3,17 +3,18 @@ unless Luffa::Environment.travis_ci?
 
     before do
       RunLoop::SimControl.terminate_all_sims
-      RunLoop::SimControl.new.reset_sim_content_and_settings
     end
 
     let(:endpoint) { 'http://localhost:37265' }
+    let(:xcode) { Resources.shared.xcode }
 
     describe '#form_factor' do
       let(:device)  do
         options = {
-              :app => Resources.shared.app_bundle_path(:lp_simple_example),
+              :app => Resources.shared.app_bundle_path(:cal_smoke_app),
               :device_target =>  device_target,
-              :sim_control => RunLoop::SimControl.new,
+              :xcode => xcode,
+              :sim_control => Resources.shared.sim_control,
               :launch_retries => Luffa::Retry.instance.launch_retries
         }
         launcher = Calabash::Cucumber::Launcher.new
@@ -25,10 +26,8 @@ unless Luffa::Environment.travis_ci?
 
       context 'device is an ipad' do
         let(:device_target) {
-          xcode = RunLoop::Xcode.new
-          version = xcode.version
           if xcode.version_gte_6?
-            Luffa::Simulator.instance.core_simulator_for_xcode_version('iPad', 'Retina', version)
+            Resources.shared.simulator_identifier_with_name('iPad Retina')
           else
             'iPad Retina (64-bit) - Simulator - iOS 7.1'
           end
@@ -36,12 +35,21 @@ unless Luffa::Environment.travis_ci?
         it { is_expected.to be == 'ipad' }
       end
 
-      context 'device is an 4in iphone' do
+      context 'iPhone 5 is an 4in iphone' do
         let(:device_target) {
-          xcode = RunLoop::Xcode.new
-          version = xcode.version
           if xcode.version_gte_6?
-            Luffa::Simulator.instance.core_simulator_for_xcode_version('iPhone', '5', version)
+            Resources.shared.simulator_identifier_with_name('iPhone 5')
+          else
+            'iPhone Retina (4-inch) - Simulator - iOS 7.1'
+          end
+        }
+        it { is_expected.to be == 'iphone 4in' }
+      end
+
+      context 'iPhone 5s is an 4in iphone' do
+        let(:device_target) {
+          if xcode.version_gte_6?
+            Resources.shared.simulator_identifier_with_name('iPhone 5s')
           else
             'iPhone Retina (4-inch) - Simulator - iOS 7.1'
           end
@@ -51,10 +59,8 @@ unless Luffa::Environment.travis_ci?
 
       context 'device is a 3.5" iphone' do
         let(:device_target) {
-          xcode = RunLoop::Xcode.new
-          version = xcode.version
           if xcode.version_gte_6?
-            Luffa::Simulator.instance.core_simulator_for_xcode_version('iPhone', '4s', version)
+            Resources.shared.simulator_identifier_with_name('iPhone 4s')
           else
             'iPhone Retina (3.5-inch) - Simulator - iOS 7.1'
           end
@@ -64,10 +70,8 @@ unless Luffa::Environment.travis_ci?
 
       context 'device is an iphone 6' do
         let(:device_target) {
-          xcode = RunLoop::Xcode.new
-          version = xcode.version
           if xcode.version_gte_6?
-            Luffa::Simulator.instance.core_simulator_for_xcode_version('iPhone', '6', version)
+            Resources.shared.simulator_identifier_with_name('iPhone 6')
           else
             # iPhone 6 does not exist on Xcode < 6
             nil
@@ -78,10 +82,8 @@ unless Luffa::Environment.travis_ci?
 
       context 'device is an iphone 6+' do
         let(:device_target) {
-          xcode = RunLoop::Xcode.new
-          version = xcode.version
           if xcode.version_gte_6?
-            Luffa::Simulator.instance.core_simulator_for_xcode_version('iPhone', '6 Plus', version)
+            Resources.shared.simulator_identifier_with_name('iPhone 6 Plus')
           else
             # iPhone 6 does not exist on Xcode < 6
             nil
