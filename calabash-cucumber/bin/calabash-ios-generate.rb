91cd0069941fb6239d52225b049d80d6ad91c900
Joshua Moody <joshuajmoody@gmail.com>
2015-10-22 19:34:22 +0200

    CLI: clean up the gen task

--- calabash-cucumber/bin/calabash-ios-generate.rb
+++ calabash-cucumber/bin/calabash-ios-generate.rb
@@ -2,22 +2,21 @@
 def calabash_scaffold
   if File.exists?(@features_dir)
     puts "A features directory already exists. Stopping..."
-    #puts "Run calabash-ios update for update instructions."
     exit 1
   end
+
   msg("Question") do
-    puts "I'm about to create a subdirectory called features."
-    puts "features will contain all your calabash tests."
-    puts "Please hit return to confirm that's what you want."
+    puts "I'm about to create a features directory here:"
+    puts ""
+    puts "#{ENV['PWD']}/features"
+    puts ""
+    puts "This directory will contain all of your calabash tests."
+    puts "Shall I proceed? (Y/n)"
   end
-  exit 2 unless STDIN.gets.chomp == ''
 
+  response = STDIN.gets.chomp.downcase
+  proceed = response == "" || response == "y"
+
+  exit 2 unless proceed
   FileUtils.cp_r(@source_dir, @features_dir)
-
-  msg("Info") do
-    puts "Features subdirectory created. \n"
-    puts "Make sure you've build your -cal scheme in XCode and "
-    puts "try executing \n\ncucumber"
-  end
-
 end
