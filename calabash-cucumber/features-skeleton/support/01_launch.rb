ba8a465f8ebabe848784e59f588206e78d659c19
Joshua Moody <joshuajmoody@gmail.com>
2015-10-23 10:42:02 +0200

    Skeleton: remove XTC notice from 01_launch
    As it is written is basically meaningless.
    
    The XTC experience should be transparent to users.
    
    The documentation for the XTC should be in the Xamarin docs

--- calabash-cucumber/features-skeleton/support/01_launch.rb
+++ calabash-cucumber/features-skeleton/support/01_launch.rb
@@ -1,20 +1,40 @@
-########################################
-#                                      #
-#       Important Note                 #
-#                                      #
-#   When running calabash-ios tests at #
-#   www.xamarin.com/test-cloud         #
-#   the  methods invoked by            #
-#   CalabashLauncher are overridden.   #
-#   It will automatically ensure       #
-#   running on device, installing apps #
-#   etc.                               #
-#                                      #
-########################################
-
 require 'calabash-cucumber/launcher'
 
-
+# http://calabashapi.xamarin.com/ios/file.ENVIRONMENT_VARIABLES.html#label-APP+and+APP_BUNDLE_PATH
+#
+# In order for Calabash to launch an app on simulator, it must have the path 
+# to the .app.  By default, Xcode builds to a DerivedData directory:
+#
+# ~/Library/Developer/Xcode/DerivedData/??/Build/Products/Debug-iphonesimulator/??.app
+#
+# Calabash tries to analyze the DerivedData directory to find your .app, but
+# there are often several possible matches.  This makes automatically detecting
+# the right .app difficult to impossible.
+#
+# If Calabash cannot find your .app or if you want to be sure that Calabash is
+# targeting the correct .app, you can set the APP variable before launching:
+#
+# $ APP=/path/to/Your.app bundle exec cucumber
+#
+# You can also do this in a config/cucumber.yml profile.
+#
+# app:       APP=/path/to/Your.app
+# default:  -p app
+#
+# $ bundle exec cucumber
+#
+# The Calabash iOS Smoke Test app has several very useful scripts for building
+# and staging .apps and .ipas to a local directory.
+#
+# * https://github.com/calabash/ios-smoke-test-app/blob/master/CalSmokeApp/bin/make/app-cal.sh
+# * https://github.com/calabash/ios-smoke-test-app/blob/master/CalSmokeApp/bin/xcode-build-phase/stage-CalSmoke-cal-products.sh
+#
+# The second script is an Xcode Run Script. It copies ipas and apps to a local
+# directory after an Xcode build:
+#
+# Products/app/Your.app
+# Products/ipa/Your.ipa
+#
 # APP_BUNDLE_PATH = "#{ENV['HOME']}/Library/Developer/Xcode/DerivedData/??/Build/Products/Calabash-iphonesimulator/??.app"
 # You may uncomment the above to overwrite the APP_BUNDLE_PATH
 # However the recommended approach is to let Calabash find the app itself
