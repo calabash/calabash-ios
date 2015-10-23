192f6f35d827f2b901fa7f64946b985e0653d2f9
Joshua Moody <joshuajmoody@gmail.com>
2015-10-23 10:46:31 +0200

    Skeleton: add links to example hooks

--- calabash-cucumber/features-skeleton/support/01_launch.rb
+++ calabash-cucumber/features-skeleton/support/01_launch.rb
@@ -1,5 +1,11 @@
 require 'calabash-cucumber/launcher'
 
+# You can find examples of more complicated launch hooks in these
+# two repositories:
+#
+# https://github.com/calabash/ios-smoke-test-app/blob/master/CalSmokeApp/features/support/01_launch.rb
+# https://github.com/calabash/ios-webview-test-app/blob/master/CalWebViewApp/features/support/01_launch.rb
+
 module Calabash::Launcher
   @@launcher = nil
 
