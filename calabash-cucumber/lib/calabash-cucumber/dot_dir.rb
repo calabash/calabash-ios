ba81c5bc226a3606cf0ccdccf1c352ac372c7718
Joshua Moody <joshuajmoody@gmail.com>
2015-11-19 16:09:46 +0100

    DotDir: rewrite with explicit Calabash module
    `<top (required)>': uninitialized constant Calabash (NameError)

--- calabash-cucumber/lib/calabash-cucumber/dot_dir.rb
+++ calabash-cucumber/lib/calabash-cucumber/dot_dir.rb
@@ -1,13 +1,14 @@
 # A module for managing the ~/.calabash directory.
-module Calabash::DotDir
-
-  def self.directory
-    home = RunLoop::Environment.user_home_directory
-    dir = File.join(home, ".calabash")
-    if !File.exist?(dir)
-      FileUtils.mkdir_p(dir)
+module Calabash
+  module DotDir
+    def self.directory
+      home = RunLoop::Environment.user_home_directory
+      dir = File.join(home, ".calabash")
+      if !File.exist?(dir)
+        FileUtils.mkdir_p(dir)
+      end
+      dir
     end
-    dir
   end
 end
 
