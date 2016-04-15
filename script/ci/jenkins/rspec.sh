889f76f040fa9edeb20d8f0082d310c71bff1d78
Joshua Moody <joshuajmoody@gmail.com>
2016-04-15 09:37:05 +0200

    Jenkins: run inject dylib integration spec

--- script/ci/jenkins/rspec.sh
+++ script/ci/jenkins/rspec.sh
@@ -7,15 +7,6 @@ rm -rf spec/reports
 rbenv exec \
   bundle exec \
   rspec \
-  spec/lib
-
-rbenv exec \
-  bundle exec \
-  rspec \
-  spec/bin/calabash_ios_sim_spec.rb
-
-rbenv exec \
-  bundle exec \
-  rspec \
-  spec/integration/device_spec.rb
+  spec/lib \
+  spec/integration/launcher/inject_dylibs_spec.rb
 
