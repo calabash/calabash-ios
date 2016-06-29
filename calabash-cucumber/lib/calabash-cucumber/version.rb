4cd4ea64e2edd48f96482335cffdb5eceac1bdd2
Joshua Moody <joshuajmoody@gmail.com>
2016-06-29 14:39:19 +0200

    Gem: bump the version to 0.19.2

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,10 +3,10 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = "0.19.1"
+    VERSION = "0.19.2"
 
     # @!visibility public
     # The minimum required version of the Calabash embedded server.
-    MIN_SERVER_VERSION = "0.19.1"
+    MIN_SERVER_VERSION = "0.19.2"
   end
 end
