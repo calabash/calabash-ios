eb9c27969b39654ee52ebcc434c2f175efab97f2
Joshua Moody <joshuajmoody@gmail.com>
2016-08-09 09:05:52 +0200

    Cucumber: add shared #wait_for_text_in_view

--- calabash-cucumber/test/cucumber/features/steps/shared.rb
+++ calabash-cucumber/test/cucumber/features/steps/shared.rb
@@ -1,3 +1,29 @@
+module TestApp
+  module Shared
+
+    def wait_for_text_in_view(text, mark)
+      query = "* marked:'#{mark}'"
+      actual = wait_for_view(query).first["text"]
+      if actual != text
+        fail %Q[
+Expected query:
+
+#{query}
+
+to return a view with the correct text:
+
+  Actual: #{actual}
+Expected: #{text}
+
+]
+      end
+    end
+
+  end
+end
+
+World(TestApp::Shared)
+
 Given(/^the app has launched$/) do
   wait_for do
     !query("*").empty?
