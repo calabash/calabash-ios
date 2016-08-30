9f7895cbfa4978925fa7d1b750a352bb368b790a
Joshua Escribano <joescr@microsoft.com>
2016-08-29 17:53:20 -0700

    Fixes swipe direction which was previously reversed

--- calabash-cucumber/lib/calabash-cucumber/gestures/coordinates.rb
+++ calabash-cucumber/lib/calabash-cucumber/gestures/coordinates.rb
@@ -288,7 +288,7 @@ module Calabash
           radius = ([element_width, element_height].min) * scale_radius_by
           to_x = x_center + (radius * Math.cos(radians))
           to_y = y_center + (radius * Math.sin(radians))
-          { :x => to_x, :y => to_y }
+          { :x => -1 * to_x, :y => -1 * to_y }
         end
 
         private
