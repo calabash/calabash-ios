a5ca6d3bf47399dafd4fbd57d14a7b9791edb080
Joshua Moody <joshuajmoody@gmail.com>
2016-09-03 22:12:03 +0200

    Test: UIA swipe needs a larger :dx for CI

--- calabash-cucumber/test/cucumber/features/steps/swipe.rb
+++ calabash-cucumber/test/cucumber/features/steps/swipe.rb
@@ -1,15 +1,24 @@
 Then(/^I can swipe to go back to the Pan menu$/) do
   x_offset = -1 * query("*")[0]["rect"]["width"] / 2.0
-  ## Uses swipe-delta for instruments since their gesture is slightly not strong enough to open menu
-  # this behavior could probably also be changed in the test app
-  swipe(:right, {:query => "*", :duration => 0.7, :force => :strong, :offset => {:x => x_offset, :y => 0}, :"swipe-delta" =>{:horizontal => {:dx => -1 * x_offset + 20, :dy => 0}}})
+  # Uses swipe-delta for instruments since their gesture is not strong enough
+  # to open menu this behavior could probably also be changed in the test app.
+  swipe(:right, {
+    :query => "*",
+    :duration => 0.7,
+    :offset => {:x => x_offset, :y => 0},
+    :force => :strong,
+
+    # This is ignored by DeviceAgent.
+    :"swipe-delta" => {:horizontal =>
+                       {:dx => -1 * x_offset + 80, :dy => 0}}
+  })
   wait_for_animations
   wait_for_view("* marked:'pan page'")
 end
 
 And(/^I swipe to delete the '(.*?)' table cell$/) do |cell_title|
   cell_query = "UILabel marked:'#{cell_title}'"
-  wait_for_view(cell_query) 
+  wait_for_view(cell_query)
   x_offset = query(cell_query)[0]["rect"]["width"] / 2.5
   swipe(:left, {:query => cell_query, :duration => 0.5, :force => :strong, :offset => {:x => x_offset, :y => 0}})
   wait_for_animations
