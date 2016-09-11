8a598ae31c386d42cc18b60ae8a157dc03976915
Joshua Moody <joshuajmoody@gmail.com>
2016-09-11 17:36:17 +0200

    Spec: use a mock World object

--- calabash-cucumber/spec/lib/uia_spec.rb
+++ calabash-cucumber/spec/lib/uia_spec.rb
@@ -1,36 +1,34 @@
-module Calabash
-  module RspecTests
-    module UIA
-      class TestObject
-        include Calabash::Cucumber::UIA
-        include Calabash::Cucumber::HTTPHelpers
-      end
-    end
-  end
-end
 
 describe Calabash::Cucumber::UIA do
 
-  let(:test_obj) { Calabash::RspecTests::UIA::TestObject.new }
+  let(:world) do
+    Class.new do
+      include Calabash::Cucumber::UIA
+      include Calabash::Cucumber::HTTPHelpers
+      include Calabash::Cucumber::WaitHelpers
+      def to_s; "#<World RSPEC STUB>"; end
+      def inspect; to_s; end
+    end.new
+  end
 
   describe 'uia_result' do
     describe "when 'status' key is 'success' returns" do
       it "value of the 'value' key when key exists" do
         value = {:key => :value }
         input = {'status' => 'success', 'value' => value, 'index' =>0}
-        expect(test_obj.send(:uia_result, input)).to be == value
+        expect(world.send(:uia_result, input)).to be == value
       end
 
       it "empty hash when 'value' key does not exist" do
         input = {'status' => 'success', 'index' =>0}
-        expect(test_obj.send(:uia_result, input)).to be == nil
+        expect(world.send(:uia_result, input)).to be == nil
       end
     end
 
     describe "when 'status' key is not 'success' returns" do
       it 'the argument it was passed' do
         input = {'status' => 'error', 'value' => nil, 'index' =>0}
-        expect(test_obj.send(:uia_result, input)).to be == input
+        expect(world.send(:uia_result, input)).to be == input
       end
     end
   end
@@ -39,35 +37,35 @@ describe Calabash::Cucumber::UIA do
     describe 'does not raise an error' do
       it "when :status == 'success' and :value is a hash" do
         mocked_value = {}
-        expect(test_obj).to receive(:uia_handle_command).and_return(mocked_value)
-        expect { test_obj.uia_type_string 'foo' }.not_to raise_error
+        expect(world).to receive(:uia_handle_command).and_return(mocked_value)
+        expect { world.uia_type_string 'foo' }.not_to raise_error
       end
 
       it "when :status == 'success' and :value is not a Hash" do
         # Output from typing in UIWebViews.
         mocked_value = ':nil'
-        expect(test_obj).to receive(:uia_handle_command).and_return(mocked_value)
-        expect { test_obj.uia_type_string 'foo' }.not_to raise_error
+        expect(world).to receive(:uia_handle_command).and_return(mocked_value)
+        expect { world.uia_type_string 'foo' }.not_to raise_error
       end
 
       it "when :status != 'success' or 'error'" do
         mocked_value = {'status' => 'unknown', 'value' => 'error message', 'index' =>0}
-        expect(test_obj).to receive(:uia_handle_command).and_return(mocked_value)
-        expect { test_obj.uia_type_string 'foo' }.not_to raise_error
+        expect(world).to receive(:uia_handle_command).and_return(mocked_value)
+        expect { world.uia_type_string 'foo' }.not_to raise_error
       end
     end
 
     describe "raises an error when :status == 'error'" do
       it 'and result has a :value key' do
         mocked_value = {'status' => 'error', 'value' => 'error message', 'index' =>0}
-        expect(test_obj).to receive(:uia_handle_command).and_return(mocked_value)
-        expect { test_obj.uia_type_string 'foo' }.to raise_error RuntimeError
+        expect(world).to receive(:uia_handle_command).and_return(mocked_value)
+        expect { world.uia_type_string 'foo' }.to raise_error RuntimeError
       end
 
       it 'and result does not have a :value key' do
         mocked_value = {'status' => 'error', 'index' =>0}
-        expect(test_obj).to receive(:uia_handle_command).and_return(mocked_value)
-        expect { test_obj.uia_type_string 'foo' }.to raise_error RuntimeError
+        expect(world).to receive(:uia_handle_command).and_return(mocked_value)
+        expect { world.uia_type_string 'foo' }.to raise_error RuntimeError
       end
     end
   end
@@ -80,10 +78,10 @@ describe Calabash::Cucumber::UIA do
           expect(launcher).to receive(:run_loop).and_return({:uia_strategy => :preferences})
           expect(Calabash::Cucumber::Launcher).to receive(:launcher_if_used).and_return(launcher)
           expect(launcher).to receive(:attached_to_automator?).and_return true
-          expect(test_obj).to receive(:http).and_return('')
+          expect(world).to receive(:http).and_return('')
 
           expect do
-            test_obj.uia('command')
+            world.uia('command')
           end.to raise_error RuntimeError
         end
       end
@@ -106,7 +104,7 @@ describe Calabash::Cucumber::UIA do
           expect(RunLoop).to receive(:send_command).and_return(run_loop_response)
 
           expect do
-            test_obj.uia('command')
+            world.uia('command')
           end.to raise_error RuntimeError
         end
 
@@ -126,7 +124,7 @@ describe Calabash::Cucumber::UIA do
             expect(RunLoop).to receive(:send_command).and_return(run_loop_response)
 
             expect do
-              test_obj.uia('command')
+              world.uia('command')
             end.to raise_error RuntimeError
           end
 
@@ -144,7 +142,7 @@ describe Calabash::Cucumber::UIA do
             expect(RunLoop).to receive(:send_command).and_return(run_loop_response)
 
             expect do
-              test_obj.uia('command')
+              world.uia('command')
             end.to raise_error('uia action failed for an unknown reason')
           end
         end
