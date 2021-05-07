29c63b36a3c8f5aeba9bbc20cfaeff24c6668841
Atul Tomar <atul.tomar@xing.com>
2021-05-07 16:52:09 +0200

    bump version

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,7 +3,7 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = "0.21.10"
+    VERSION = "0.23.0"
 
     # @!visibility public
     # The minimum required version of the Calabash embedded server.
