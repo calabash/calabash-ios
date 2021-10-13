9ac7b77c03d3d77215c79d421caca32fb3d0ee5e
Ilya Bausov <69856845+IlyaBausovAkvelon@users.noreply.github.com>
2021-10-13 18:43:45 +0300

    Release 0.23.1 (#1444)
    * Release 0.23.1
    
    Co-authored-by: Atul Tomar <atul.tomar@xing.com>
    Co-authored-by: Serghei Moret <joesatriani323@gmail.com>

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,10 +3,10 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = "0.22.2"
+    VERSION = "0.23.1"
 
     # @!visibility public
     # The minimum required version of the Calabash embedded server.
-    MIN_SERVER_VERSION = "0.22.2"
+    MIN_SERVER_VERSION = "0.23.0"
   end
 end
