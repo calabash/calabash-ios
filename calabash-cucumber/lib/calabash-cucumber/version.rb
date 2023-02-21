58c0d1c4f52c6e7f7b026991f25c7050c35f5c86
Massimiliano De Vivo <massimiliano.devivo>
2023-02-21 09:16:39 +0100

    Bump versions

--- calabash-cucumber/lib/calabash-cucumber/version.rb
+++ calabash-cucumber/lib/calabash-cucumber/version.rb
@@ -3,10 +3,10 @@ module Calabash
 
     # @!visibility public
     # The Calabash iOS gem version.
-    VERSION = "0.23.5"
+    VERSION = "0.23.6"
 
     # @!visibility public
     # The minimum required version of the Calabash embedded server.
-    MIN_SERVER_VERSION = "0.23.5"
+    MIN_SERVER_VERSION = "0.23.6"
   end
 end
